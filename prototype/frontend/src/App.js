83c6d3d8f6e9a66156a690ebbc5e17325aff2e8b
Khoa Ly <kly27@calpoly.edu>
2022-02-26 22:16:58 -0800

    Fixed component import error

--- prototype/frontend/src/App.js
+++ prototype/frontend/src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 
-import Home from './components/About/About.js'
-import About from './components/Home/Home.js'
+import Home from './components/Home/Home.js'
+import About from './components/About/About.js'
 import Contact from './components/Contact/Contact.js'
 import Login from './components/Login/Login.js'
 
