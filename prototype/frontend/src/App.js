d83a18394676a6ef14a5c980e297ac61ae7eba46
Khoa Ly <kly27@calpoly.edu>
2022-02-27 15:47:42 -0800

    Images loading now

--- prototype/frontend/src/App.js
+++ prototype/frontend/src/App.js
@@ -5,7 +5,10 @@ import Home from './components/Home/Home.js'
 import About from './components/About/About.js'
 import Contact from './components/Contact/Contact.js'
 import Login from './components/Login/Login.js'
-import Splash from './components/Splash/Splash/index'
+import Splash from './components/Splash/Frame12/index'
+
+import Logo from './components/Splash/Frame12/infounion--1@2x.png'
+import Drop from "./components/Splash/Frame12/drop-down-box-3@2x.png"
 
 import './App.css';
 
@@ -20,11 +23,29 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
         <Route path="login" element={<Login />} />
-          <Route path="search" element={<Splash />} />
-
+          <Route path="search" element={<Splash splashProps={frame12Data.splashProps}/>} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
+const splashData = {
+  infounion: Logo,
+  spanText1: "I am a/an",
+  occupationDropDown: Drop,
+  spanText2: "Occupation",
+  spanText3: "based in",
+  locationDropDown: Drop,
+  spanText4: "Location",
+  spanText5: "I want",
+  informationDropDown: Drop,
+  spanText6: "Information",
+  frame6: "./components/Splash/Frame12/frame-6-1@2x.png",
+  frame7: "./components/Splash/Frame12/img/frame-7-1@2x.png",
+};
+
+const frame12Data = {
+  splashProps: splashData,
+};
