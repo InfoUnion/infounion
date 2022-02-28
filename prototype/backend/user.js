56885c77e0e2591e084b060c33178bb1b2d3d2ac
Justin Scabarozi <justinscabarozi@Giselles-MacBook-Air.local>
2022-02-28 15:44:20 -0800

    Added Schemas

--- prototype/backend/user.js
+++ prototype/backend/user.js
@@ -1,19 +1,53 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
   name: {
     type: String,
     required: true,
     trim: true,
   },
-  job: {
+  location: String,
+  occupation: String,
+  union_favorites: [mongoose.ObjectId],
+  comments: [mongoose.ObjectId],
+  union_membership: [mongoose.ObjectId],
+}, {collection : 'users_list'});
+
+const CommentSchema = new mongoose.Schema({
+  comment: String,
+  rating: Number,
+  user_id: Number,
+  union_id: Number,
+  reply_id: [mongoose.ObjectId],
+}, {collection : 'comments_list'});
+
+const UnionSchema = new mongoose.Schema({
+  name: {
     type: String,
     required: true,
     trim: true,
-    validate(value) {
-      if (value.length < 2) throw new Error("Invalid job.");
-    },
   },
-}, {collection : 'users_list'});
+  location: String,
+  description: String,
+  member_count: Number,
+  industry: String,
+  year_founded: Number,
+  website: String,
+  comment_id: [mongoose.ObjectId],
+  specific_id: Number,
+}, {collection : 'unions_list'});
 
-module.exports = UserSchema;
+const SpecificSchema = new mongoose.Schema({
+  information_query: String,
+  spending: Number,
+  salary: Number,
+  assets: Number,
+  benefits: [String],
+  strikes_and_events: [String],
+  crime_and_corruption: [String],
+  links: [String],
+}, {collection : 'specifics_list'});
+
+module.exports = {UserSchema,CommentSchema,UnionSchema,SpecificSchema};
