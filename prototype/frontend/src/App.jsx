12cd853f01d60a0f8a9dd51d581579e371ff9459
giselleium <77294804+giselleium@users.noreply.github.com>
2022-02-28 21:11:04 -0800

    Simple Footer Added

--- prototype/frontend/src/App.jsx
+++ prototype/frontend/src/App.jsx
@@ -1,16 +1,15 @@
 import {Routes, Route, Link, Router, Switch} from "react-router-dom";
 //import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import logo from './logo.svg';
+import './App.css';
 
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
 import Login from './components/Login/Login'
 import Splash from './components/Splash/Splash'
-import NavBar from "./components/Navbar/Navbar";
-
-import './App.css';
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -24,7 +23,7 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="splash" element={<Splash />} />
       </Routes>
-
+        <Footer/>
     </div>
   );
 }
---
+++ prototype/frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import {  Link } from "react-router-dom";
+import "./Footer.css"
+import React from "react";
+
+const Footer= () =>{
+    return (
+        <footer className="footer">
+            <p> Copyright 2022 - InfoUnion & Associates </p>
+        </footer>
+
+    );
+}
+export default Footer;
