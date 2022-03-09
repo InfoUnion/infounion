6f7e823b76a75988a9ab5ba07a1f5ea37afe51a0
Kareem Darghous <kareem@Kareems-MacBook-Pro.local>
2022-03-09 11:15:11 -0800

    first map commit

---
+++ prototype/frontend/src/App.js
@@ -0,0 +1,90 @@
+import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
+import './App.css';
+import { Test } from './components/Union_List_Page/Union_List_Page.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to InfoUnion!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="login" element={<Login />} />
+        <Route path="test" element={<Test />} />
+      </Routes>
+    </div>
+  );
+}
+
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>This is our homepage!</h2>
+        <p>We're here to inform our workers.</p>
+      </main>
+      <nav>
+        <p><Link to="/about">About</Link></p>
+        <p><Link to="/contact">Contact Us</Link></p>
+        <p><Link to="/login">Log in</Link></p>
+        <p><Link to="/test">Test</Link></p>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          InfoUnion was created with one idea in mind: info on all things job-related, easy to understand, all in one location.
+          Our development team is composed of four geniuses based in San Luis Obispo, CA.
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+function Contact() {
+  return (
+    <>
+      <main>
+        <h2>Contact Us!</h2>
+        <p>
+          Email us at InfoUnion@calpoly.edu
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+function Login() {
+  return (
+    <>
+      <main>
+        <h2>Log in to your InfoUnion account</h2>
+        <p>
+          Webpage currently under construction!
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+export default App;
+
---
+++ prototype/frontend/src/components/Union_List_Page/Union_List_Page.js
@@ -0,0 +1,31 @@
+import { Routes, Route, Link } from "react-router-dom";
+import '../../App.css';
+import { Home } from '../../App.js';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+
+function Test() {
+  return (
+ 
+<MapContainer center={[35.282753, -120.659615]} zoom={13}>
+  
+    
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[35.282753, -120.659615]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+  <Marker position={[38, -120.659615]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+
+</MapContainer>
+  );
+}
+  export {Test};
