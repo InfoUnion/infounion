2cc84bcb63a2c61e01012d074e81ba1ca791b9b6
Justin Scabarozi <justinscabarozi@Giselles-Air.lan>
2022-03-10 17:35:26 -0800

    cloud update

--- prototype/backend/README.md
+++ prototype/backend/README.md
@@ -7,3 +7,9 @@ npm install --save-dev nodemon
 npm install cors
 npm install mongoose
 npm install jest
+npm install dotenv
+
+
+# local files:
+
+Make a .env file in this folder
--- prototype/backend/user-services.js
+++ prototype/backend/user-services.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');
-const {UserSchema,CommentSchema,UnionSchema,SpecificSchema} = require("./user");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const UserSchema = require("./user");
+const dotenv = require("dotenv");
+dotenv.config();
 
 let dbConnection;
 
 function getDbConnection() {
+    const uri = process.env.MONGO_URI;
+    //const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    /*client.connect(err => {
+        const collection = client.db("test").collection("devices");
+        // perform actions on the collection object
+        client.close();
+    });*/
+
     if (!dbConnection) {
-        dbConnection = mongoose.createConnection("mongodb://localhost:27017/users", {
+        dbConnection = mongoose.createConnection(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
