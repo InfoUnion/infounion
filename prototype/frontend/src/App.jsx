4b2f2791be72fabf8ad9b961f48ad77488bd0b6b
Khoa Ly <kly27@calpoly.edu>
2022-03-12 00:53:27 -0800

    Added login / register pages

--- prototype/frontend/src/App.jsx
+++ prototype/frontend/src/App.jsx
@@ -2,11 +2,12 @@ import { Routes, Route, Link, Router, Switch } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
-import Home from './components/Home/Home'
-import About from './components/About/About'
-import Contact from './components/Contact/Contact'
-import Login from './components/Login/Login'
-import Splash from './components/Splash/Splash'
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Splash from './components/Splash/Splash';
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
         <Route path="splash" element={<Splash />} />
       </Routes>
       <Footer />
