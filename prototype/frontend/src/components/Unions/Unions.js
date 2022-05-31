5787883c04a3de30ca8104191050df49013f41df
Khoa Ly <kly27@calpoly.edu>
2022-05-30 20:02:21 -0700

    Added dropdowns to unions

--- prototype/frontend/src/components/Unions/Unions.js
+++ prototype/frontend/src/components/Unions/Unions.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { Container, Box, Stack } from '@mui/material'
+import { Container, Box, Stack, } from '@mui/material'
+import ComboBox from '../Splash/ComboBox'
 
 import CollapsibleTable from './CollapsibleTable';
 import { useLocation } from 'react-router-dom';
@@ -8,9 +9,15 @@ import { useLocation } from 'react-router-dom';
 import './Unions.css'
 
 function Unions() {
-  const location = useLocation();
-  const u_state = location.state;
-  
+  const u_location = useLocation();
+  const u_state = u_location.state;
+
+  const [occupation, setOccupation] = React.useState('')
+  const occupations = ['Teacher', 'Lawyer', 'Engineer']
+
+  const [location, setLocation] = React.useState('')
+  const locations = ['California', 'New York', 'Texas']
+
   return (
     <Container maxWidth='xl'>
       <Box
@@ -22,7 +29,16 @@ function Unions() {
           <div className='wrapper'>
             <h1>Unions</h1>
           </div>
-          <CollapsibleTable loc = {u_state}/>
+          <Stack
+            direction='row'
+            spacing={1}
+          >
+            <ComboBox list={occupations} label='Occupation' value={occupation} setValue={setOccupation} />
+            <br />
+            <ComboBox list={locations} label='Location' value={location} setValue={setLocation} />
+          </Stack>
+          <br />
+          <CollapsibleTable loc={u_state} />
         </Stack>
       </Box>
     </Container>
