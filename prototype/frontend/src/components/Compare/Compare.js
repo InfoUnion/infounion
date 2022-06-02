3c8a863da23135e0dcd4914eb56aa2ddf88eddd3
Tomas Prieto <tomasprieto@Tomass-MacBook-Pro-3.local>
2022-06-02 13:56:50 -0700

    Small compare style changes

--- prototype/frontend/src/components/Compare/Compare.js
+++ prototype/frontend/src/components/Compare/Compare.js
@@ -9,16 +9,16 @@ import {
   TableCell,
   TableRow,
   Typography,
-  Container, 
+  Container,
   Stack,
   Grid
 } from '@mui/material'
 import MultiBox from '../MultiBox/MultiBox';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 //import unions from '../assets/data/unions.jsx';
 
 
-function Compare () {
+function Compare() {
 
   const [unions, setUnions] = React.useState([]);
   const [u, setU] = React.useState();
@@ -74,48 +74,49 @@ function Compare () {
         <h4>Select up to three unions to compare side-by-side</h4>
       </div>
 
-      
+
       <Stack
         direction='row'
         spacing={1}
-        sx={{ margin: 1}}
+        sx={{ margin: 1 }}
       >
-        <MultiBox 
-        list={u ? u.length < 3 ? unions : [] : unions} 
-        label='Select Union' 
-        value={''} 
-        setValue={handleUnionSelect}/>
+        <MultiBox
+          list={u ? u.length < 3 ? unions : [] : unions}
+          label='Select Union'
+          value={''}
+          setValue={handleUnionSelect} />
         {u ? u.map((uni) => {
           const row = uni
-        return(
-          <Box sx={{height: 500, width: 325, border: 1, }}>
-          <Grid container height={150} width ={325} paddingTop={1} borderBottom={1} alignContent="center" textAlign="center">
-          <Typography variant='h6' gutterBottom component='div' fontWeight={"bold"}>
-          <Link to={`${uni.name}`} state={{ u: { row } }}>{uni.name}</Link>
-          </Typography>
-          </Grid>
-          <Grid container paddingLeft={2} paddingRight={1} paddingTop={1}>
-          <Typography>
-          <b>Telephone:</b> {uni.phone ? uni.phone : 'Unavailable'}
-        </Typography>
-        <Typography>
-          <b>Address: </b>  
-          {uni.address.streetAddress ? uni.address.streetAddress
-          + ' ' + uni.address.addressLocality + ', ' + uni.address.addressRegion + ' ' + uni.address.postalCode : 'Unavailable'}
-        </Typography>
-        <Typography>
-          <b>Founded in:</b> {uni.founded ? uni.founded : 'Unavailable'}
-        </Typography>
-        <Typography>
-          <b>Number of Employees:</b> {uni.numEmp ? uni.numEmp : 'Unavailable'}
-        </Typography>
-        <Typography>
-          <b>Description:</b> {uni.description ? uni.description : 'Unavailable'}
-        </Typography>
-        </Grid>
-        </Box>
+          return (
+            <Box sx={{ height: 500, width: 325, border: 1, }}>
+              <Grid container height={150} width={325} paddingTop={1} borderBottom={1} alignContent="center" textAlign="center">
+                <Typography variant='h6' gutterBottom component='div' fontWeight={"bold"}>
+                  <Link to={`${uni.name}`} state={{ u: { row } }}>{uni.name}</Link>
+                </Typography>
+              </Grid>
+              <Grid container paddingLeft={2} paddingRight={1} paddingTop={1}>
+                <Typography>
+                  <b>Telephone:</b> {uni.phone ? uni.phone : 'Unavailable'}
+                </Typography>
+                <Typography>
+                  <b>Address: </b>
+                  {uni.address.streetAddress ? uni.address.streetAddress
+                    + ' ' + uni.address.addressLocality + ', ' + uni.address.addressRegion + ' ' + uni.address.postalCode : 'Unavailable'}
+                </Typography>
+                <Typography>
+                  <b>Founded in:</b> {uni.founded ? uni.founded : 'Unavailable'}
+                </Typography>
+                <Typography>
+                  <b>Number of Employees:</b> {uni.numEmp ? uni.numEmp : 'Unavailable'}
+                </Typography>
+                <Typography>
+                  <b>Description:</b> {uni.description ? uni.description : 'Unavailable'}
+                </Typography>
+              </Grid>
+            </Box>
 
-        )}
+          )
+        }
         ) : null}
       </Stack>
     </Container>
